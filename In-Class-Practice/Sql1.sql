e798896d54942937a2199cb9f4cf42e0772e88f0
novak727 <devinkennedy727@gmail.com>
2026-01-19 13:45:55 -0600

    Create Sql1.sql

---
+++ In-Class-Practice/Sql1.sql
@@ -0,0 +1,14 @@
+# CREATE DATABASE Sql1;
+# USE Sql1;
+
+# SELECT * FROM address;
+# SELECT * FROM person;
+
+
+# SELECT lastName, firstName, city, state FROM person LEFT JOIN address ON person.personId = address.personId
+
+# SELECT lastName, firstName, city, state FROM person RIGHT JOIN address ON person.personId = address.personId
+
+SELECT lastName, firstName, city, state FROM person LEFT JOIN address ON person.personId = address.personId
+UNION
+SELECT lastName, firstName, city, state FROM person RIGHT JOIN address ON person.personId = address.personId
