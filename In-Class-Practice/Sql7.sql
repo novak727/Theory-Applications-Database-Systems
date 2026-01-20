5cb43fe19bc7ca25c907a8e30f3845c2e9750725
novak727 <devinkennedy727@gmail.com>
2026-01-20 16:11:51 -0600

    Update Sql7.sql

--- In-Class-Practice/Sql7.sql
+++ In-Class-Practice/Sql7.sql
@@ -1,7 +1,7 @@
-#CREATE DATABASE Sql7;
-#USE Sql7;
+CREATE DATABASE Sql7;
+USE Sql7;
 
-#SELECT * FROM article_view;
+SELECT * FROM article_view;
 
 SELECT DISTINCT author_id as id FROM article_view
 WHERE author_id = viewer_id
