a652f07659db45e296c14a449e6ab2228012cabb
novak727 <devinkennedy727@gmail.com>
2026-01-20 16:11:41 -0600

    Update Sql6.sql

--- In-Class-Practice/Sql6.sql
+++ In-Class-Practice/Sql6.sql
@@ -1,11 +1,11 @@
-#CREATE DATABASE Sql6;
-#USE Sql6;
+CREATE DATABASE Sql6;
+USE Sql6;
 
-#SELECT * FROM company;
+SELECT * FROM company;
 # com_id, name, city
-# SELECT * FROM orders;
+SELECT * FROM orders;
 # order_id, order_date, com_id, sales_id, amount
-#SELECT * FROM salesperson;
+SELECT * FROM salesperson;
 # sales_id, name, salary, commission_rate, hire_date
 
 SELECT name FROM salesperson
@@ -14,4 +14,3 @@ WHERE name NOT IN
 LEFT JOIN orders ON salesperson.sales_id = orders.sales_id
 LEFT JOIN company ON orders.com_id = company.com_id
 WHERE company.name = 'RED');
-#WHERE company.name IS NULL;
