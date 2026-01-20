d26ba25fa61599d658b3f584c1c0874442091af6
novak727 <devinkennedy727@gmail.com>
2026-01-20 16:11:02 -0600

    Update Sql4.sql

--- In-Class-Practice/Sql4.sql
+++ In-Class-Practice/Sql4.sql
@@ -1,11 +1,11 @@
-# CREATE DATABASE Sql4;
-# USE Sql4;
+CREATE DATABASE Sql4;
+USE Sql4;
 
-# SELECT * FROM bonus;
-# id & bonus
+SELECT * FROM bonus;
+id & bonus
 
-# SELECT * from employee;
-# id, name, supervisor, salary
+SELECT * from employee;
+id, name, supervisor, salary
 
 SELECT name, bonus FROM employee LEFT JOIN bonus ON bonus.id = employee.id WHERE bonus < 1000 OR bonus IS NULL;
  
