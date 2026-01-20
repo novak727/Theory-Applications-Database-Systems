0eb41127af666938589aa41d347fbd9474aec1c9
novak727 <devinkennedy727@gmail.com>
2026-01-20 16:10:32 -0600

    Update Sql2.sql

--- In-Class-Practice/Sql2.sql
+++ In-Class-Practice/Sql2.sql
@@ -1,10 +1,10 @@
-# CREATE DATABASE Sql2
-# USE Sql2
+CREATE DATABASE Sql2
+USE Sql2
 
-# SELECT * FROM customers;
-# id=orders.CustomerId & name
+SELECT * FROM customers;
+id=orders.CustomerId & name
 
-# SELECT * FROM orders;
-# id and CustomerId=customers.id
+SELECT * FROM orders;
+id and CustomerId=customers.id
 
 SELECT name as Customers FROM customers WHERE id NOT IN (SELECT customerId FROM orders);
