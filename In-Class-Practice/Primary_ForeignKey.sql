02da55a85ce6d561730db9dcef0a2af83b5e2fb2
novak727 <devinkennedy727@gmail.com>
2026-01-20 16:09:04 -0600

    Update Primary_ForeignKey.sql

--- In-Class-Practice/Primary_ForeignKey.sql
+++ In-Class-Practice/Primary_ForeignKey.sql
@@ -1,53 +1,53 @@
-#CREATE DATABASE Primary_ForeignKey;
-#USE Primary_ForeignKey;
+CREATE DATABASE Primary_ForeignKey;
+USE Primary_ForeignKey;
 
-#CREATE TABLE Student_Details(
-#		Student_ID INT PRIMARY KEY,
-#        Student_Name VARCHAR(30),
-#        Student_Address VARCHAR(50)
-#);
+CREATE TABLE Student_Details(
+		Student_ID INT PRIMARY KEY,
+        Student_Name VARCHAR(30),
+        Student_Address VARCHAR(50)
+);
 
-#INSERT INTO Student_Details (Student_ID, Student_Name, Student_Address)
-#VALUES
-#(1, 'John Smith', 'New York'),
-#(2, 'Emma Brown', 'Los Angeles'),
-#(3, 'Michael Lee', 'Chicago');
+INSERT INTO Student_Details (Student_ID, Student_Name, Student_Address)
+VALUES
+(1, 'John Smith', 'New York'),
+(2, 'Emma Brown', 'Los Angeles'),
+(3, 'Michael Lee', 'Chicago');
 
-#SELECT * FROM Student_Details;
+SELECT * FROM Student_Details;
 
-#CREATE TABLE Grades(
-#		Student_ID INT,
-#        Grade CHAR(1),
-#        FOREIGN KEY (Student_ID) REFERENCES Student_Details(Student_ID)
-#);
+CREATE TABLE Grades(
+		Student_ID INT,
+        Grade CHAR(1),
+        FOREIGN KEY (Student_ID) REFERENCES Student_Details(Student_ID)
+);
 
-#INSERT INTO Grades (Student_ID, Grade)
-#VALUES
-#	(1, 'A'),
-#    (2, 'B'),
-#    (3, 'A');
+INSERT INTO Grades (Student_ID, Grade)
+VALUES
+	(1, 'A'),
+    (2, 'B'),
+    (3, 'A');
 
-#SELECT * FROM Grades;
+SELECT * FROM Grades;
 
-# This should not work
-#INSERT INTO Grades (Student_ID, Grade)
-#VALUES
-#	(4, 'F'),
-#    (5, 'F');
+ This should not work
+INSERT INTO Grades (Student_ID, Grade)
+VALUES
+	(4, 'F'),
+    (5, 'F');
 
-#CREATE TABLE Student_Course(
-#		ID INT,
-#        CourseID VARCHAR(10),
-#        Semester VARCHAR(15),
-#        Grade CHAR(1),
-#        PRIMARY KEY(ID, CourseID)
-#);
+CREATE TABLE Student_Course(
+		ID INT,
+        CourseID VARCHAR(10),
+        Semester VARCHAR(15),
+        Grade CHAR(1),
+        PRIMARY KEY(ID, CourseID)
+);
 
-#INSERT INTO Student_Course(ID, CourseID, Semester, Grade)
-#VALUES
-#	(101, 'CSE 101', 'Fall 2025', 'A'),
-#    (101, 'MTH 101', 'Fall 2025', 'B'),
-#    (102, 'CSE 101', 'Fall 2025', 'B'),
-#    (103, 'CSE 101', 'Fall 2025', 'A');
+INSERT INTO Student_Course(ID, CourseID, Semester, Grade)
+VALUES
+	(101, 'CSE 101', 'Fall 2025', 'A'),
+    (101, 'MTH 101', 'Fall 2025', 'B'),
+    (102, 'CSE 101', 'Fall 2025', 'B'),
+    (103, 'CSE 101', 'Fall 2025', 'A');
 
 SELECT * FROM Student_Course;
